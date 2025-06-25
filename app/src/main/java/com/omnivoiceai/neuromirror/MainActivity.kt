1d563435f426bd3306087ad0c1179a2df512b529
thedarkknight197 <michele.vitiellobonaventura@gmail.com>
2025-06-25 16:02:59 +0200

    set default theme violet

--- app/src/main/java/com/omnivoiceai/neuromirror/MainActivity.kt
+++ app/src/main/java/com/omnivoiceai/neuromirror/MainActivity.kt
@@ -6,16 +6,20 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LifecycleEventEffect
 import com.omnivoiceai.neuromirror.ui.theme.NeuroMirrorTheme
+import com.omnivoiceai.neuromirror.ui.theme.Purple80
 
 private const val TAG = "MainActivity"
 
@@ -27,12 +31,20 @@ class MainActivity : ComponentActivity() {
         setContent {
             NeuroMirrorTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Scaffold(
+                    containerColor = MaterialTheme.colorScheme.background,
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                ) { innerPadding ->
+                    Surface(
+                        color = Color.Transparent,
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    ) {
+                        Greeting("Android")
+                    }
                 }
+
             }
         }
     }
