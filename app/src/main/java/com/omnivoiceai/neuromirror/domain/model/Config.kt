eef746b8146fbd1c08d2dae50e19d8fd2d60700e
DIGITALNEXT | Michele Vitiello Bonaventura <info@mvbdev.cloud>
2025-07-11 18:25:31 +0200

    improved app configuration

--- app/src/main/java/com/omnivoiceai/neuromirror/domain/model/Config.kt
+++ app/src/main/java/com/omnivoiceai/neuromirror/domain/model/Config.kt
@@ -3,7 +3,16 @@ package com.omnivoiceai.neuromirror.domain.model
 import com.omnivoiceai.neuromirror.BuildConfig
 
 object Config {
-    val BASE_URL = if(BuildConfig.DEBUG) "http://192.168.1.226:8000/" else "https://api.ai.digitalnext.business/"
+
+    const val FORCE_REMOTE_URL = true
+
+    private const val LOCAL_URL = "http://192.168.1.226:8000/"
+    private const val REMOTE_URL = "https://api.ai.digitalnext.business/"
+    val BASE_URL = when {
+        FORCE_REMOTE_URL -> REMOTE_URL
+        BuildConfig.DEBUG -> LOCAL_URL
+        else -> REMOTE_URL
+    }
     const val DATABASE_NAME = "note-list"
     const val PREFERENCE_DATA_STORE = "note-list"
 }
